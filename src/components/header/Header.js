bc3b77fc60e0f38304ce5be570af2559a059876e
Sakil Mahmud <sakilahmmad71@gmail.com>
2021-03-02 01:20:54 +0600

    Project setup for testing

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import '../../styles/components/Header.scss';
+
+const Header = () => {
+  return (
+    <div className="wrapper">
+      <header className="headerComponent">
+        <nav className="navbar">
+          <div className="navigation">
+            <h1 className="logo">Hello World!</h1>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Header
